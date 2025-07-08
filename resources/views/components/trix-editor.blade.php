3316e76b53153d23c3e267bf1cb48777aa10be17
Alex Bordei <alex@whiz.ro>
2025-07-08 07:03:52 +0300

    Disable file uploads in Trix editor component

--- resources/views/components/trix-editor.blade.php
+++ resources/views/components/trix-editor.blade.php
@@ -1,8 +1,19 @@
 <div>
     <label class="label block mb-2" for="{{ $name }}">{{ $label }}</label>
     <input id="{{ $name }}" type="hidden" name="{{ $name }}" value="{{$value}}">
-    <trix-editor class="form-control trix-content {{ $error ? 'is-invalid' : '' }}" input="{{ $name }}" rows="{{ $rows ?? 5 }}"></trix-editor>
+    
+    <trix-editor 
+        class="form-control trix-content {{ $error ? 'is-invalid' : '' }}" 
+        input="{{ $name }}" 
+        rows="{{ $rows ?? 5 }}" 
+        data-trix-allow-files="false">
+    </trix-editor>
+    
     @if($error || $success)
-        <small class="block mt-2 {{ $error ? 'invalid-feedback' : 'valid-feedback' }}">{{ $error ?? $success }}</small>
+        <small class="block mt-2 {{ $error ? 'invalid-feedback' : 'valid-feedback' }}">
+            {{ $error ?? $success }}
+        </small>
     @endif
-</div> 
+</div>
+
+
